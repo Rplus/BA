62e0f27a06a9bc2dfb9e0022cbedd9e3017cb2df
Rplus <rplus.tw@gmail.com>
2023-08-16 03:06:53 +0800

    separate i18n wordings

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import data_url from './data/students.json?url'
   import Student from './lib/Student.svelte'
+  import { words } from './i18n.js'
 
   let filterProps = [
     // 'SquadType',
@@ -132,35 +133,6 @@
   function _(str = '') {
     return words[str] || str;
   }
-
-  let words = {
-    Explosion: '爆發',
-    Pierce: '貫穿',
-    Mystic: '神秘',
-    Sonic: '振動',
-    Main: '前排',
-    Support: '後排',
-
-    TacticRole: '作用',
-    Position: '位置',
-    BulletType: '攻屬',
-    ArmorType: '防屬',
-
-    DamageDealer: '輸出',
-    Tanker: '坦克',
-    Supporter: '輔助',
-    Healer: '治療',
-    Vehicle: 'T.S.',
-
-    Back: '遠距',
-    Front: '近距',
-    Middle: '中距',
-
-    LightArmor: '輕甲',
-    HeavyArmor: '重甲',
-    Unarmed: '特殊',
-    ElasticArmor: '彈力',
-  };
 
   function formReset() {
     filterName = '';
---
+++ src/i18n.js
@@ -0,0 +1,28 @@
+export const words = {
+  Explosion: '爆發',
+  Pierce: '貫穿',
+  Mystic: '神秘',
+  Sonic: '振動',
+  Main: '前排',
+  Support: '後排',
+
+  TacticRole: '作用',
+  Position: '位置',
+  BulletType: '攻屬',
+  ArmorType: '防屬',
+
+  DamageDealer: '輸出',
+  Tanker: '坦克',
+  Supporter: '輔助',
+  Healer: '治療',
+  Vehicle: 'T.S.',
+
+  Back: '遠距',
+  Front: '近距',
+  Middle: '中距',
+
+  LightArmor: '輕甲',
+  HeavyArmor: '重甲',
+  Unarmed: '特殊',
+  ElasticArmor: '彈力',
+};
